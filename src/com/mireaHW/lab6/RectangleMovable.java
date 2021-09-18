2a8153327a90f994daeebcb3871bf3a5d0beff79
vitaliks007 <45079744+vitaliks007@users.noreply.github.com>
2021-09-18 09:27:32 +0300

    Lab6

--- src/com/mireaHW/lab6/RectangleMovable.java
+++ src/com/mireaHW/lab6/RectangleMovable.java
@@ -5,6 +5,11 @@ public class RectangleMovable extends Rectangle implements Movable {
         super(rightUp, leftDown);
     }
 
+    public boolean checkMove(int x, int y) {
+        return (this.getLeftDown().getX() - x == this.getRightUp().getX() - x) &&
+                (this.getRightUp().getY() - y == this.getLeftDown().getY() - y);
+    }
+
     @Override
     public void move(int x, int y) {
         setRightUp( new Point(this.getRightUp().getX() + x, this.getRightUp().getY() + y));
